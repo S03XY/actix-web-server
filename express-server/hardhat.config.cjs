49f37e4233742e1c3be2d2253fee18fe786c7da1
Shashank Pandey <prishankofficial@gmail.com>
2024-04-05 16:38:20 +0530

    hardhat config

--- express-server/hardhat.config.cjs
+++ express-server/hardhat.config.cjs
@@ -2,5 +2,37 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    arbitrum: {
+      url: process.env.ARBITRUM,
+      accounts: [process.env.DEPLOYER],
+    },
+    avail: {
+      url: process.env.AVAIL,
+      accounts: [process.env.DEPLOYER],
+    },
+    gnosis: {
+      url: process.env.GNOSIS,
+      accounts: [process.env.DEPLOYER],
+    },
+    morph: {
+      url: process.env.MORPH,
+      accounts: [process.env.DEPLOYER],
+    },
+    neon: {
+      url: process.env.NEON,
+      accounts: [process.env.DEPLOYER],
+    },
+  },
+  etherscan: {},
 };
--- express-server/index.js
+++ express-server/index.js
@@ -2,11 +2,6 @@ import express from "express";
 
 const app = express();
 
-
-
-
-
-
 app.listen(3001, () => {
   console.log("listening on http://localhost:3001");
 });
